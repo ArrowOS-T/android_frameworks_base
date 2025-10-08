2379abc6d1aa50261e76c0b0cb22d67635a6de96
Iverz <asterixiverz@gmail.com>
2025-10-08 16:05:22 +0000

    base: Drop inline frameworks spoof
    Drop PropImitationHooks and FeatureHooksUtils, for stability purpose.
    
    Signed-off-by: Iverz <asterixiverz@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -208,7 +208,6 @@ import com.android.server.tv.tunerresourcemanager.TunerResourceManagerService;
 import com.android.server.twilight.TwilightService;
 import com.android.server.uri.UriGrantsManagerService;
 import com.android.server.usage.UsageStatsService;
-import com.android.server.utils.PropImitationService;
 import com.android.server.utils.TimingsTraceAndSlog;
 import com.android.server.vibrator.VibratorManagerService;
 import com.android.server.vr.VrManagerService;
@@ -3087,11 +3086,6 @@ public final class SystemServer implements Dumpable {
         } catch (Throwable e) {
             reportWtf("starting System UI", e);
         }
-        t.traceEnd();
-
-        // PropImitiationService
-        t.traceBegin("PropImitationService");
-        mSystemServiceManager.startService(PropImitationService.class);
         t.traceEnd();
 
         t.traceEnd(); // startOtherServices
